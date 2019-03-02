a96c7016d58d095c5d80535a59e163946f63934f
agnoster <354651432@qq.com>
2019-03-02 15:18:07 +0800

    重置密码

--- src/views/User/ResetPwd.vue
+++ src/views/User/ResetPwd.vue
@@ -5,12 +5,13 @@
             <form class="form-horizontal">
                 <div class="form-group">
                     <div class="col-sm-12">
-                        <input type="tel" class="form-control" placeholder="请输入6-12位密码，支持英文字母与数字">
+                        <input type="password" v-model="password" class="form-control"
+                               placeholder="请输入6-12位密码，支持英文字母与数字">
                     </div>
                 </div>
                 <div class="form-group">
                     <div class="col-sm-12">
-                        <input type="password" class="form-control" placeholder="再次输入新密码">
+                        <input type="password" v-model="password1" class="form-control" placeholder="再次输入新密码">
                     </div>
                 </div>
                 <div class="tips">
@@ -30,7 +31,7 @@
                     </div>
                 </div>
 
-                <button type="submit" class="blue-btn">完成</button>
+                <button type="submit" @click.prevent="resetpwd()" class="blue-btn">完成</button>
 
             </form>
         </div>
@@ -40,10 +41,43 @@
 </template>
 
 <script>
+    import axios from 'axios'
+    import qs from 'querystring'
+
     export default {
         data() {
             return {
-                msg: null
+                msg: null,
+                password: null,
+                password1: null,
+                phone: this.$store.getters.userInfo.phone
+            }
+        },
+        methods: {
+            resetpwd() {
+                if (this.password != this.password1) {
+                    this.msg = '两次密码不一致';
+                    return;
+                }
+                this.sendCode(p => {
+                    axios.post('/edu/user/changePassword', qs.stringify({
+                        phone: this.phone,
+                        code: p.content,
+                        password: this.password
+                    })).then(p => {
+                        if (p.data.httpCode == 200) {
+                            this.$router.push('/user/success')
+                        } else {
+                            this.msg = "接口调用失败"
+                        }
+                    })
+                })
+            },
+            sendCode(callback) {
+                let phone = this.phone;
+                axios.post('/edu/user/sendShortMessage', qs.stringify({phone})).then(p => {
+                    callback(p.data)
+                })
             }
         }
     }
