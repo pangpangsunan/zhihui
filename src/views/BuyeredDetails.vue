aaf3c8d14b40b8810a961d3f41e7b98827daa220
agnoster <354651432@qq.com>
2019-04-26 22:03:58 +0800

    处理没有qrcode的情况

--- src/views/BuyeredDetails.vue
+++ src/views/BuyeredDetails.vue
@@ -35,8 +35,7 @@
                 <li v-show="current==='page1'">
                     <div class="notices">
                         <p v-for="item in message.data">{{ item.content }}</p>
-                        <img :src="course.course.qrcode" style="width: 7.5rem;height: 7.5rem; margin-top: 3rem"
-                             v-if="course.course&&course.course.qrcode">
+                        <img :src="course.qrcode" style="width: 7.5rem;height: 7.5rem; margin-top: 3rem">
                     </div>
                 </li>
                 <li v-show="current==='page2'">
