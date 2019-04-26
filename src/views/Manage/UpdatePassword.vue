150dbd1d4bada31282ea891469370066148139e4
agnoster <354651432@qq.com>
2019-04-26 21:26:28 +0800

    修改密码前发短信验证

--- src/views/Manage/UpdatePassword.vue
+++ src/views/Manage/UpdatePassword.vue
@@ -1,6 +1,27 @@
 <template>
     <div>
-        <div class="page2">
+        <div class="page1" v-if="page=='page1'">
+            <form class="form-horizontal" @submit.prevent="bindphone()">
+                <div class="form-group">
+                    <div class="col-sm-12">
+                        <input type="text" v-model="userInfo.phone" class="form-control" placeholder="请输入手机号" required>
+                    </div>
+                </div>
+                <div class="form-group">
+                    <div class="col-sm-8">
+                        <input type="text" class="form-control" v-model="valCode" placeholder="验证码" required
+                               pattern="\d{4}">
+                    </div>
+                    <sendcode :phone="userInfo.phone" @success="valCodeTrue=$event"></sendcode>
+                </div>
+                <div class="tips">
+                    <span v-if="msg">{{msg}}</span>
+                </div>
+                <button type="submit" class="orange-btn">绑定</button>
+
+            </form>
+        </div>
+        <div class="page2" v-if="page=='page2'">
             <p>请设置新密码</p>
             <div class="bindphone-page">
                 <form class="form-horizontal" @sumit.prevent="updatepwd">
@@ -54,17 +75,36 @@
 
                 })
             },
+            bindphone() {
+                if (this.valCode == this.valCodeTrue) {
+                    this.page = 'page2';
+                    return;
+                }
+                alert("短信验证码错误");
+            },
         },
         data() {
             return {
                 msg: null,
                 password1: null,
                 password: null,
+                page: 'page1',
+                phone: '',
+                valCode: null,
+                valCodeTrue: null,
+            }
+        },
+        created() {
+            if (!this.userInfo.phone) {
+                this.$router.push('/manage/bindphone')
             }
         },
         computed: mapGetters([
             'userInfo'
-        ])
+        ]),
+        components: {
+            sendcode: () => import('@/components/sendcode.vue'),
+        }
     }
 </script>
 
